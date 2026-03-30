ac0e63dee6a37fe96499ed3b287caf7fcf0b8b19
Rafael Alves <faelalves957@gmail.com>
2026-03-30 09:56:36 -0300

    Quarta questão

---
+++ Questão_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() { 
+    int x, y;
+printf("Digite dois números:");
+scanf("%i %i", &x, &y);
+printf("Soma: %i \nproduto: %i \ndivisão: %i", x+y,x*y,x/y);
+return 0;
+
+}
